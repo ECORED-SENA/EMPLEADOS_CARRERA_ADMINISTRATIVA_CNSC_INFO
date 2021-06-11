097d2eb3d383f89318ca52379ae072cbad48740c
Luis Perez <ljpm9401@gmail.com>
2021-06-10 19:32:15 -0500

    Faltan creditos y video

---
+++ src/config/titulo.js
@@ -0,0 +1,1 @@
+module.exports = 'Curso para empleados de carrera administrativa sujetos al sistema tipo de evaluación de la CNSC'
---
+++ src/views/Inicio.vue
@@ -0,0 +1,159 @@
+<template lang="pug">
+section
+  .banner-principal.mb-5
+    .container.tarjeta.p-4.p-sm-5(
+      :style="{'background-image': `url(${require('@/assets/curso/fondo-banner-principal.png')})`}"
+    )
+      .row.justify-content-around.align-items-center
+        .col-lg-7.col-xxl-6.banner-principal__info
+          .banner-principal__programa
+            .h1.mb-0 Curso para empleados de carrera administrativa sujetos al sistema tipo de evaluación de la CNSC
+          
+          .h2 Información del programa
+          .banner-principal__datos
+            ul
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Nombre del programa: 
+                p.banner-principal__datos__item__texto Curso para empleados de carrera administrativa sujetos al sistema tipo de evaluación de la CNSC
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Código: 
+                p.banner-principal__datos__item__texto 41740023
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Total Horas: 
+                p.banner-principal__datos__item__texto 48 Horas
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Duración en meses: 
+                p.banner-principal__datos__item__texto 1
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Modalidad: 
+                p.banner-principal__datos__item__texto virtual
+              li.mb-3.banner-principal__datos__item
+                p.h6.mb-0.me-3.banner-principal__datos__item__titulo Requisito de ingreso: 
+                .banner-principal__datos__item__texto
+                  ul.lista-ul
+                    li
+                      i.lista-ul__vineta
+                      p Nivel académico adecuado para caracterizar al aspirante de acuerdo con el perfil de egreso.
+                    li
+                      i.lista-ul__vineta
+                      p No requiere formación para el trabajo y desarrollo humano.
+                    li
+                      i.lista-ul__vineta
+                      p Edad mínima definida por ley: 15 años.
+                    li
+                      i.lista-ul__vineta
+                      p No existe legislación que establezca restricciones de ingreso.
+                    li
+                      i.lista-ul__vineta
+                      p Aspectos actitudinales, motivacionales o de interés: dinámico en el trabajo tanto en ambientes abiertos como cerrados, propositivo y de fácil adaptación a cambios tecnológicos; de fácil comunicación y con dotes de conciliador frente a conflictos laborales; receptivo ante las órdenes y nuevos direccionamientos de las empresas.
+                    li
+                      i.lista-ul__vineta
+                      p Se requiere que el estudiante en Ambiente Virtual de Aprendizaje – AVA tenga un manejo adecuado de los elementos básicos relacionados con herramientas informáticas y de comunicación como correo electrónico, chat, procesadores de texto, hojas de cálculo, software para presentaciones, Internet, navegadores y otras herramientas tecnológicas necesarias para la formación virtual.
+
+        .d-none.d-lg-block.col-lg-5
+          //- img(src="@/assets/curso/banner-princiapal.svg")
+          .video
+            | <iframe data-v-1f9e4816="" width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
+    .titulo__template--a.mb-4
+      span.h4 1. Presentación
+
+    p Apreciado aprendiz bienvenido al programa de formación Curso para empleados de carrera administrativa sujetos al sistema tipo de evaluación de la CNSC, el cual le permite conocer desde el rol evaluado el procedimiento que se establece a partir del conocimiento de los aspectos generales y elementos del marco normativo del sistema de evaluación de la CNSC.
+      br
+      br
+      | El programa se desarrolla a través de tres unidades temáticas: marco normativo del sistema de evaluación de la CNSC, aspectos generales del sistema tipo de evaluación de la CNSC y fases de la evaluación del desempeño laboral, orientado con recursos educativos digitales y estrategias de aprendizaje de tipo individual y experiencial, que permiten la apropiación de conceptos y alcanzar de manera significativa los resultados de aprendizaje a través de las actividades propuestas.
+      br
+      br
+      | Este curso brinda conocimientos y destrezas para el alcance de la competencia asociada “Evaluar desempeño de acuerdo con metodologías y normativa”, a su vez, se estructuran elementos que permiten establecer los propósitos de cada una de las fases del proceso de evaluación del desempeño laboral.
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
+    .titulo__template--a.mb-4
+      span.h4 2. Justificación<br>del programa
+
+    p La formación complementaria del Curso para empleados de carrera administrativa sujetos al sistema tipo de evaluación de la CNSC que se desarrolla a través del Servicio Nacional de Aprendizaje - SENA tiene como objetivo reconocer desde el rol de evaluado, la evaluación del desempeño como herramienta de gestión, en la que los empleados públicos de carrera administrativa y en período de prueba deben ajustar sus acciones y conductas, mediante la articulación de los compromisos funcionales y comportamentales con las metas de la dependencia, para responder a las necesidades y expectativas de la ciudadanía y al logro del cumplimiento de objetivos de la entidad.
+
+    p.mt-3 A partir del 1° de febrero de 2019 se deroga el Acuerdo 565 de 2016 de la Comisión Nacional del Servicio Civil (CNSC) y entra en vigor el Acuerdo 617 de 2018, expedido por la misma Comisión, el cual establece el Sistema tipo de evaluación de desempeño laboral para los servidores de carrera administrativa y en período de prueba mediante la aplicación de diferentes fases y con la participación de diversos actores.
+
+    p.mt-3 El nuevo acuerdo contempla cambios con respecto a lo definido en el Acuerdo 565: cuatro fases en lugar de cinco para llevar a cabo el proceso de evaluación, indica que el responsable de la conformación de la Comisión evaluadora es el Jefe de la Unidad de Personal o quien haga sus veces, cambian también los períodos de concertación de compromisos, períodos de seguimiento, los porcentajes de los compromisos laborales, en los cuales se debe tener en cuenta que “la evaluación de gestión por áreas o dependencias no se tendrá en cuenta en el marco del proceso de calificación; sin embargo, se considera un insumo para la concertación de compromisos”.
+
+    p.mt-3 En la escala de los compromisos comportamentales se ajustaron las descripciones y los resultados numéricos por nivel de desarrollo; en cuanto a niveles de cumplimiento, el nivel destacado fue suspendido por el Consejo de Estado. Como instrumento de evaluación se cuenta con un aplicativo diseñado por la CNSC, reemplazando los once formatos en Excel que se tenían definidos para llevar a cabo el proceso de evaluación del desempeño.
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
+    .titulo__template--a.mb-4
+      span.h4 3. Competencias <br> a desarrollar
+
+    p #[strong 210201045.] Evaluar desempeño de acuerdo con metodologías y normativa.
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
+    .titulo__template--a.mb-4
+      span.h4 4. Perfil <br> de ingreso
+
+    ul.lista-ul.mt-3
+      li
+        i.lista-ul__vineta
+        p Nivel académico adecuado para caracterizar al aspirante de acuerdo con el perfil de egreso.
+      li
+        i.lista-ul__vineta
+        p No requiere formación para el trabajo y desarrollo humano.
+      li
+        i.lista-ul__vineta
+        p Edad mínima definida por ley: 15 años.
+      li
+        i.lista-ul__vineta
+        p No existe legislación que establezca restricciones de ingreso.
+      li
+        i.lista-ul__vineta
+        p Aspectos actitudinales, motivacionales o de interés: dinámico en el trabajo tanto en ambientes abiertos como cerrados, propositivo y de fácil adaptación a cambios tecnológicos; de fácil comunicación y con dotes de conciliador frente a conflictos laborales; receptivo ante las órdenes y nuevos direccionamientos de las empresas.
+      li
+        i.lista-ul__vineta
+        p Se requiere que el estudiante en Ambiente Virtual de Aprendizaje – AVA tenga un manejo adecuado de los elementos básicos relacionados con herramientas informáticas y de comunicación como correo electrónico, chat, procesadores de texto, hojas de cálculo, software para presentaciones, Internet, navegadores y otras herramientas tecnológicas necesarias para la formación virtual.
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
+    .titulo__template--a.mb-4
+      span.h4 5. Perfil <br> de egreso
+
+    p El egresado del programa de formación complementario estará en capacidad de identificar los aspectos generales del Sistema de evaluación tipo de la CNSC conforme a la normatividad vigente, así como, reconocer las fases del proceso de evaluación.  En general, el egresado de este programa complementario, será una persona con capacidad para proponer compromisos laborales y de mejoramiento dentro del proceso de evaluación desde el rol de evaluado.
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
+    .titulo__template--a.mb-4
+      span.h4 6. Estrategia <br> metodológica
+
+    p La estrategia metodológica propuesta para el presente curso está centrada en la construcción de autonomía para garantizar la calidad en la educación en el marco de la formación por competencias, soportadas en la utilización de las Tecnologías de la Información y la Comunicación (TIC) y en ambientes virtuales de aprendizaje, con una estrategia de aprendizaje individual para la apropiación de conceptos y, el método de casos para alcanzar aprendizajes significativos, lo anterior se da a través de la transición de fases que permiten un recorrido desde la identificación de la actividad a partir de un caso, luego se busca compartir los resultados y reacciones a través de los compromisos propuestos, continuo a esto, descubrir por medio del análisis una posible solución y, se finaliza con conectar la experiencia al contexto real, vinculando al aprendiz con la realidad cotidiana y con el desarrollo de las competencias en este ámbito.
+
+    p.mt-3 Igualmente, debe estimular de manera permanente la autocrítica y la reflexión del aprendiz sobre el quehacer y los resultados de aprendizaje que logra a través de la vinculación activa de las cuatro fuentes de información para la construcción de conocimiento:
+
+    ul.lista-ul.mt-3
+      li
+        i.lista-ul__vineta
+        p El instructor - tutor
+      li
+        i.lista-ul__vineta
+        p El entorno
+      li
+        i.lista-ul__vineta
+        p Las TIC
+      li
+        i.lista-ul__vineta
+        p El trabajo colaborativo
+
+    
+  //- Créditos
+  .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5
+    .titulo__template--a.mb-4
+      span.h4 Créditos
+
+    Creditos
+
+  Footer
+
+</template>
+<script>
+export default {
+  name: 'Inicio',
+  data: () => ({}),
+}
+</script>
+
+<style lang="sass"></style>
